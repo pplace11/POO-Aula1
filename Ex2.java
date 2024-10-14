72838c29c4b2eb631d6861548183c340d3aed6fa
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:01:46 +0100

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,14 @@
+package aula2;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        int a = 3;
+        double w = 5;
+        char s = 'd';
+        boolean q = true;
+        System.out.println(w);
+    }
+
+
+}
