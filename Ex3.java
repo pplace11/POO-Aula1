10824ebb4af8066919cb9afe874a1f47db90ff4f
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:01:51 +0100

    Create Ex3.java

---
+++ Ex3.java
@@ -0,0 +1,9 @@
+package aula2;
+public class Ex3 {
+    public static void main(String[] args) {
+        int i1 = 8;
+        double d2 = (double)i1;
+        System.out.println(i1);
+    }
+    
+}
